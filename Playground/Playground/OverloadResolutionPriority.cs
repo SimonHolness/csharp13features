2f5cbd6998a4f68964204b5d506c9c91e527070a
Simon Holness <simon.holness@red-gate.com>
2024-11-26 15:33:09 +0000

    Add OverloadResolutionPriority to that original class

--- Playground/Playground/OverloadResolutionPriority.cs
+++ Playground/Playground/OverloadResolutionPriority.cs
@@ -1,17 +1,22 @@
-﻿namespace Playground;
+﻿using System.Runtime.CompilerServices;
+
+namespace Playground;
 
 public class OverloadResolutionPriority
 {
     public static void MyMethod(params int[] numbers)
     {
+        Console.WriteLine("Using the old way");
         foreach (var number in numbers)
         {
             Console.WriteLine(number);
         }
     }
     
+    [OverloadResolutionPriority(1)]
     public static void MyMethod(params IEnumerable<int> numbers)
     {
+        Console.WriteLine("Using the new way");
         foreach (var number in numbers)
         {
             Console.WriteLine(number);
